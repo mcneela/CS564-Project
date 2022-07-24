d38df4fe7fb42c470502250e9dad0de0278d079a
Daniel McNeela <daniel.mcneela@gmail.com>
2022-07-23 21:21:07 -0500

    created jobposting model views

--- backend/jobposting/views.py
+++ backend/jobposting/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from jobposting.models import (
+    Job,
+    Company,
+    Industry,
+    Location,
+    Requirements,
+)
+from jobposting.serializers import (
+    JobSerializer,
+    CompanySerializer,
+    IndustrySerializer,
+    LocationSerializer,
+    RequirementsSerializer
+)
 # Create your views here.
+
+class JobViewSet(viewsets.ModelViewSet):
+    queryset = Job.objects.all().order_by('job_id')
+    serializer_class = JobSerializer
+
+class CompanyViewSet(viewsets.ModelViewSet):
+    queryset = Company.objects.all()
+    serializer_class = CompanySerializer
+
+class IndustryViewSet(viewsets.ModelViewSet):
+    queryset = Industry.objects.all()
+    serializer_class = IndustrySerializer
+
+class LocationViewSet(viewsets.ModelViewSet):
+    queryset = Location.objects.all()
+    serializer_class = LocationSerializer
+
+class RequirementsViewSet(viewsets.ModelViewSet):
+    queryset = Requirements.objects.all()
+    serializer_class = RequirementsSerializer
