56d2cb15e1755bbcc91eb6e0d3eb34a065575929
Daniel McNeela <daniel.mcneela@gmail.com>
2022-07-31 19:51:56 -0500

    change to bulk_create

--- backend/backend/management/commands/load_db.py
+++ backend/backend/management/commands/load_db.py
@@ -19,6 +19,7 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         used_job_ids = {}
         path = kwargs['path']
+        req_list, industries, companies, jobs, locations = [], [], [], [], []
         with open(path, 'r') as infile:
             reader = csv.reader(infile, delimiter=',')
             for i, row in enumerate(reader):
@@ -45,28 +46,42 @@ class Command(BaseCommand):
                     function,
                     fraudulent
                 ) = row
-                industry_obj, created = Industry.objects.get_or_create(name=industry)
-                company_obj, created = Company.objects.get_or_create(
+                # industry_obj, created = Industry.objects.get_or_create(name=industry)
+                industry_obj = Industry(name=industry)
+                industries.append(industry_obj)
+                company_obj = Company(
                     profile=company_profile.strip(),
                     has_logo=bool(has_company_logo),
                     industry=industry_obj
                 )
+                companies.append(company_obj)
+                # company_obj, created = Company.objects.get_or_create(
+                #     profile=company_profile.strip(),
+                #     has_logo=bool(has_company_logo),
+                #     industry=industry_obj
+                # )
                 try:
                     country, state, city = location.split(",")
                     country, state, city = country.strip(), state.strip(), city.strip()
                 except:
                     country, state, city = None, None, None
-                location_obj, created = Location.objects.get_or_create(
+                # location_obj, created = Location.objects.get_or_create(
+                #     city=city,
+                #     state=state,
+                #     country=country
+                # )
+                location_obj = Location(
                     city=city,
                     state=state,
                     country=country
                 )
+                locations.append(location_obj)
                 try:
                     salary_min, salary_max = salary_range.split("-")
                     salary_min, salary_max = int(salary_min), int(salary_max) 
                 except:
                     salary_min, salary_max = None, None
-                job_obj, created = Job.objects.get_or_create(
+                job_obj = Job(
                     title=title.strip(),
                     function=function.strip(),
                     salary_min=salary_min,
@@ -80,14 +95,42 @@ class Command(BaseCommand):
                     posted_by=company_obj,
                     located_in=location_obj
                 )
+                jobs.append(job_obj)
+                # job_obj, created = Job.objects.get_or_create(
+                #     title=title.strip(),
+                #     function=function.strip(),
+                #     salary_min=salary_min,
+                #     salary_max=salary_max,
+                #     department=department.strip(),
+                #     benefits=benefits.strip(),
+                #     description=description.strip(),
+                #     fraudulent=bool(fraudulent),
+                #     telecommuting=bool(telecommuting),
+                #     has_question=bool(has_questions),
+                #     posted_by=company_obj,
+                #     located_in=location_obj
+                # )
                 if not job_obj.job_id in used_job_ids:
                     used_job_ids[job_obj.job_id] = True
-                    requirements_obj, created = Requirements.objects.get_or_create(
+                    # requirements_obj, created = Requirements.objects.get_or_create(
+                    #     description=requirements.strip(),
+                    #     education=required_education.strip(),
+                    #     experience=required_experience.strip(),
+                    #     employment_type=employment_type.strip(),
+                    #     job=job_obj
+                    # )
+                    requirements_obj = Requirements(
                         description=requirements.strip(),
                         education=required_education.strip(),
                         experience=required_experience.strip(),
                         employment_type=employment_type.strip(),
                         job=job_obj
                     )
+                    req_list.append(requirements_obj)
+            Requirements.objects.bulk_create(req_list)
+            Location.objects.bulk_create(locations)
+            Industry.objects.bulk_create(industry)
+            Job.objects.bulk_create(jobs)
+            Company.objects.bulk_create(companies)
 
 print("Loaded database successfully!")
