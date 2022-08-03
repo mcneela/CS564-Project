a4a5dbdb517ae74e8dbb1ba6cac5e035a849c325
Daniel McNeela <daniel.mcneela@gmail.com>
2022-08-03 12:22:24 -0500

    delete search bar comments

--- frontend/src/components/SearchBar.js
+++ frontend/src/components/SearchBar.js
@@ -88,16 +88,6 @@ class SearchBar extends React.Component {
               <input className={classes.textField} onChange={this.handleSetKeywords} id="searchField"
                          label="Search for a job."
               />
-              {/* <select value={this.state.n} onChange={this.handleSetN}>
-                <option value={""}>10000</option>
-                <option value={10}>10</option>
-                <option value={20}>20</option>
-                <option value={50}>50</option>
-                <option value={100}>100</option>
-                <option value={200}>200</option>
-                <option value={500}>500</option>
-                <option value={1000}>1000</option>
-              </select> */}
               <button type="submit">Search</button>
             </form>
           </div>
