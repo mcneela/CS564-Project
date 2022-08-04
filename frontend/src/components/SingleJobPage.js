de4fde1b9f353f3007ac369b0c62530ce1a3f8de
Daniel McNeela <daniel.mcneela@gmail.com>
2022-08-03 20:04:04 -0500

    added more details to job page

--- frontend/src/components/SingleJobPage.js
+++ frontend/src/components/SingleJobPage.js
@@ -15,16 +15,25 @@ class SingleJobPage extends React.Component {
       'isLoading': true,
       'jobID': props.params.jobID,
       'jobData': null,
+      'reqData': null,
       'apiUrl': 'http://127.0.0.1:8000/api/v1/jobs/',
+      'reqUrl': 'http://127.0.0.1:8000/api/v1/requirements/',
     }
   }
 
   componentDidMount() {
     axios.get(this.state.apiUrl + this.state.jobID)
       .then((response) => {
-      this.setState({
-        jobData: response.data,
-        isLoading: false
+        this.setState({
+          jobData: response.data,
+      });
+      axios.get(this.state.reqUrl + this.state.jobID)
+        .then((response) => {
+          console.log(response.data);
+          this.setState({
+            reqData: response.data,
+            isLoading: false,
+        });
       });
     });
   }
@@ -34,6 +43,7 @@ class SingleJobPage extends React.Component {
       return <div></div>
     }
     let job = this.state.jobData;
+    let req = this.state.reqData;
     var location = '';
     if (job.located_in.city != null && job.located_in.city != '') {
       location += job.located_in.city + ', ';
@@ -48,9 +58,15 @@ class SingleJobPage extends React.Component {
       <div>
         <h1>{job.title}</h1>
         <h2>{`Location: ${location}`}</h2>
-        <p><b>Company: </b>{job.posted_by.profile}</p>
+        <p><b>Function: </b>{job.function}</p>
+        <p><b>Industry: </b>{job.posted_by.industry.name}</p>
+        <p><b>Company Profile: </b>{job.posted_by.profile}</p>
         <p><b>Description: </b>{job.description}</p>
         <p><b>Benefits: </b>{job.benefits}</p>
+        <p><b>Overall Requirements: </b>{req.description}</p>
+        <p><b>Required Education: </b>{req.education}</p>
+        <p><b>Required Experience: </b>{req.experience}</p>
+        <p><b>Employment Type: </b>{req.employment_type}</p>
       </div>
     );
   }
